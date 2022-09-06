813cf8d4b5a0a44c7dd009b278d284f5b6a64a22
Derek Antrican <deantric@microsoft.com>
2022-09-06 13:54:08 -0700

    Added additional helpful comments

--- Clockwork/Examples/ExampleTask2.cs
+++ Clockwork/Examples/ExampleTask2.cs
@@ -5,6 +5,8 @@ namespace Clockwork.Core
         public Interval Interval => new Interval(TimeType.Second, 10);
 
         //Optional: you can add a constructor to do any one-time setup when the instance is created
+        //or you can add Setup() or Teardown() methods that will be called on each run. Additionally,
+        //you can implement the Catch(Exception e) method to handle exceptions.
 
         public void Run()
         {
