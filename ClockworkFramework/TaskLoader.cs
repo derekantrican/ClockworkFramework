a3e75395949d8862fea35ea7032c119a53a2d96c
Derek Antrican <derekantrican@gmail.com>
2023-01-16 14:04:03 -0800

    Added a todo note

--- ClockworkFramework/TaskLoader.cs
+++ ClockworkFramework/TaskLoader.cs
@@ -107,6 +107,8 @@ namespace ClockworkFramework
             return exampleClasses.SelectMany(t => LoadTaskMethodsFromClassType(t));
         }
 
+        //Todo: rework some of the below (loading TaskMethods *then* filtering to ones on IClockworkTaskBase classes) so we can output a warning
+        //  for [TaskMethod]s that are on a class that doesn't inherit from IClockworkTaskBase
         public static IEnumerable<MethodInfo> LoadTaskMethodsFromClassType(Type type)
         {
             return type.GetMethods().Where(m => m.GetCustomAttribute(typeof(TaskMethodAttribute)) != null);
